ce37837fa252b68fde1d799241ec08a2be5af491
Rossiitaa <rosario.scuotto@outlook.it>
2022-09-27 16:21:33 +0200

    feat: added guest.home + Vue

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::middleware('auth')
@@ -28,3 +24,7 @@ Route::middleware('auth')
     Route::resource('posts', 'PostsController');
     Route::resource('tags', 'TagsController');
 });
+
+Route::get("{any?}", function () {
+    return view('guest.home');
+})->where("any", ".*");
